e225e0254e5967d441f0666ffcc68b02898a0ea9
Jeremiah Flaga <j.flaga@arcanys.com>
2020-07-21 16:18:42 +0800

    MassTransit getting started tutorial

---
+++ 2020-07-20-masstransit-getting-started/GettingStarted/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading.Tasks;
+using MassTransit;
+
+namespace GettingStarted
+{
+    public class Message
+    { 
+        public string Text { get; set; }
+    }
+
+    public class Program
+    {
+        public static async Task Main()
+        {
+            // await UsingInMemory.Execute();
+            await UsingRabbitMq.Execute();
+            
+        }
+    }
+}
---
+++ 2020-07-20-masstransit-getting-started/GettingStarted/UsingInMemory.cs
@@ -0,0 +1,33 @@
+
+using System;
+using System.Threading.Tasks;
+using MassTransit;
+
+namespace GettingStarted
+{
+  public class UsingInMemory
+    {
+        public static async Task Execute()
+        {
+            var bus = Bus.Factory.CreateUsingInMemory(sbc =>
+            {
+                sbc.ReceiveEndpoint("test_queue", ep =>
+                {
+                    ep.Handler<Message>(context =>
+                    {
+                        return Console.Out.WriteLineAsync($"Received: {context.Message.Text}");
+                    });
+                });
+            });
+
+            await bus.StartAsync(); // This is important!
+
+            await bus.Publish(new Message{Text = "Hi"});
+            
+            Console.WriteLine("Press any key to exit");
+            await Task.Run(() => Console.ReadKey());
+            
+            await bus.StopAsync();
+        }
+    }
+}
