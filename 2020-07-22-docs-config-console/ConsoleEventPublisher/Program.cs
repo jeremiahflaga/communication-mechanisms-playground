a5819324c96fc1019febe63bc8bde20a8696a8fd
Jeremiah Flaga <j.flaga@arcanys.com>
2020-07-22 14:10:40 +0800

    docs -> Configuration -> Console App sample

---
+++ 2020-07-22-docs-config-console/ConsoleEventPublisher/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using EventContracts;
+using MassTransit;
+
+﻿namespace EventContracts
+{
+    public interface ValueEntered
+    {
+        string Value { get; }
+    }
+}
+
+namespace ConsoleEventPublisher
+{
+    class Program
+    {
+        
+        public static async Task Main()
+        {
+            var busControl = Bus.Factory.CreateUsingRabbitMq();
+
+            var source = new CancellationTokenSource(TimeSpan.FromSeconds(10));
+
+            await busControl.StartAsync(source.Token);
+            try
+            {
+                do
+                {
+                    string value = await Task.Run(() =>
+                    {
+                        Console.WriteLine("Enter message (or quit to exit)");
+                        Console.Write("> ");
+                        return Console.ReadLine();
+                    });
+
+                    if("quit".Equals(value, StringComparison.OrdinalIgnoreCase))
+                        break;
+
+                    await busControl.Publish<ValueEntered>(new
+                    {
+                        Value = value
+                    });
+                }
+                while (true);
+            }
+            finally
+            {
+                await busControl.StopAsync();
+            }
+        }
+    }
+}
